25754e03bdd0a0c3ce77f97f5d52772d90082cae
DevGrupoNinja <desenvolvimento@gruponinja.com.br>
2026-03-02 20:24:42 +0000

    Alterações - deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For development, allow all. Change in production.
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
